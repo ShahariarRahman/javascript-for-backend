ba4b0920d97bccc36a245aad5453a9da0eedad2c
ShahariarRahman <shahariarrahman98@gmail.com>
2023-03-02 04:06:05 +0600

    consume Promise

--- Promises and Handling Multiple Promises/code/promise.js
+++ Promises and Handling Multiple Promises/code/promise.js
@@ -1,3 +1,18 @@
+console.log("1st block");
+
 const myPromise = new Promise((resolve, reject) => {
-  // any async task
+  const user = { id: 1 };
+  if (!user) {
+    reject("Something went wrong!");
+  } else {
+    setTimeout(() => {
+      resolve("Successfully got the data");
+    }, 1000);
+  }
 });
+
+myPromise
+  .then((res) => console.log("found in then:", res))
+  .catch((err) => console.log("found in error:", err));
+
+console.log("last block");
